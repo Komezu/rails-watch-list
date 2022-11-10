52ccf7a142a4e510fccee42ac9c4b6562b75884c
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-10 14:35:57 -0500

    coded bookmarks controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
 
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
